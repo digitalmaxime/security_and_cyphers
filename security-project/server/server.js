c6327f81085c550af007531c2da5fcb2a9733547
digitalmaxime <maxime.rheault@gmail.com>
2022-08-23 10:05:56 -0400

    server cors

--- security-project/server/server.js
+++ security-project/server/server.js
@@ -1,30 +1,35 @@
 // const http = require('http');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 
 const app = express();
 
+// use it before all route definitions
+app.use(cors());
+// app.use(cors({origin: '*'}));
+
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:4200');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'localhost:4200');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 
 // router.get('/localhost/', (req, res, next)=> {
@@ -38,6 +43,13 @@ app.use(function (req, res, next) {
 //     console.log("POST : ")
 // });
 
+app.get("/", (req, res) => {
+    console.log(req.url);
+    console.log(req.body);
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(`{"message": "This is a JSON response"}`);
+});
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
